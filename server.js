3fc24f2e626882fc75cd5b95504d2b92bd288606
thaArcadeGuy <gilbertsilver64@gmail.com>
2025-06-17 20:36:16 +0300

    fix: upddate server configuration for deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/users.route.js";
 import todosRouter from "./routes/todos.route.js";
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
@@ -18,14 +18,22 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/todos", todosRouter);
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.json({ message: "Todo API is running" });
 });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something broke!"});
+})
 
 
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log("connected to the database", process.env.MONGODB_URI);
+    console.log("connected to the database");
     app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    console.log(err);
+    process.exit(1)
+  });
